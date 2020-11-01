0f4e7bacd2f98fb8decbc07d51b12b1c2e1fe867
Carlos Gonzalez <cyberpolin@gmail.com>
2020-10-31 19:14:30 -0600

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '../styles/skeleton/skeleton.scss'
 import '../styles/globals.sass'
+import Router from "next/router";
+
+import withGA from "next-ga";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withGA("UA-89409450-1", Router)(MyApp);
